3b0bc4bc50bec9f5edca3741bf27c124d4e1fde2
MakinFantasy <beingp0z1t1v3@gmail.com>
2023-04-25 16:51:56 +0300

    finished?

---
+++ decompose/src/components/Search.js
@@ -0,0 +1,14 @@
+/* Поиск */
+
+import React from "react";
+
+export default function Search() {
+    return (
+        <React.Fragment>
+            <form>
+                <input type="text" className="input-text" />
+                <input type="button" value="Найти" />
+            </form>
+        </React.Fragment>
+    );
+}
